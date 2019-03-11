ecc29f539c743537cf416bd78c78fdda6e2a10e6
karen1au <kagemikaka@hotmail.com>
2019-03-11 17:33:49 +0000

    added print lowercase callback

--- callbacks/print_lowercase.js
+++ callbacks/print_lowercase.js
@@ -1,10 +1,12 @@
-var getHTML = require('./http-functions');
+var getHTML = require('../http-functions');
 
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step5.html'
 };
 
-function printHTML (html) {
-  console.log(html);
+function printLower (html) {
+  console.log(html.toLowerCase());
 }
+
+getHTML(requestOptions, printLower)
