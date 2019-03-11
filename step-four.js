2e720ad8c52a3d1caa6a5e6dad968c8b0a4c3687
karen1au <kagemikaka@hotmail.com>
2019-03-11 16:59:16 +0000

    step 4 finished, added call back

---
+++ step-four.js
@@ -0,0 +1,26 @@
+function getHTML (options, callback) {
+
+  var https = require('https');
+
+  https.get(options, function (response){
+
+    response.on('data', function (data){
+      var chunk = '';
+        chunk += data;
+      callback(chunk);
+
+    });
+  });
+
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+getHTML(requestOptions, printHTML)
